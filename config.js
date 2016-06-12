60416912bee10e7a9e211a2b9449df75804b97ab
Emile Choghi <emchoghi@gmail.com>
2016-06-12 01:28:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ var config = {};
 
 config.db = {};
 
-config.webhost = 'http://parv.us/';
+config.webhost = 'http://localhost:3000/';
 
-config.db.user = 'echoghi';
-config.db.password = 'potac877';
-config.db.host = '@ds047622.mlab.com:47622';
+config.db.user = '******';
+config.db.password = '******';
+config.db.host = '******************';
 config.db.name = 'parvus';
 
 module.exports = config;
